258ab8cc6e20f93b07079d8f5650e0843971c4e5
Praveen <49310101+Praveensreedhar@users.noreply.github.com>
2023-02-27 10:45:59 +0530

    Update latency_check.py

--- latency_check.py
+++ latency_check.py
@@ -2,9 +2,9 @@ from tcp_latency import measure_latency
 import os
 
 os.chdir("C:\\Users\\dragon\\Desktop\\Latency-Check")
-urlss = open('urls.txt')
+urls = open('urls.txt')
 
-for url in urlss:
+for url in urls:
     #latency = measure_latency(host=url)
     #measure_latency(host=url, port=443)
     url = url.strip() # Remove trailing newline character
